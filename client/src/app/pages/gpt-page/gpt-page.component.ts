c8b545a63a0e313d57d1b31c8cd1e2db1ed8b6fc
Veillette87 <alexandre.veillette@polymtl.ca>
2023-07-21 20:31:36 -0400

    add page and set routing

---
+++ client/src/app/pages/gpt-page/gpt-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gpt-page',
+  templateUrl: './gpt-page.component.html',
+  styleUrls: ['./gpt-page.component.scss']
+})
+export class GptPageComponent {
+
+}
