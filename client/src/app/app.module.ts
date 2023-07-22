44f6df5ef05b5b514dde3b796c572362d8f66852
Veillette87 <alexandre.veillette@polymtl.ca>
2023-07-21 20:14:58 -0400

    Revert "add page and set routing"
    This reverts commit 802681844df7c4c594cfeab30d39f1c2a75fe02d.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './modules/app-routing.module';
-import { AppComponent } from './pages/app/app.component';
-//import { HomePageComponent } from '@app/pages/home-page/home-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
-import { GptPageComponent } from './pages/gpt-page/gpt-page.component';
-import { ChatBoxComponent } from './components/chat-box/chat-box.component';
-import { ChatBoxMessageComponent } from './components/chat-box-message/chat-box-message.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    NavbarComponent,
-    AboutUsPageComponent,
-    GptPageComponent,
-    ChatBoxComponent,
-    ChatBoxMessageComponent,
+    AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
