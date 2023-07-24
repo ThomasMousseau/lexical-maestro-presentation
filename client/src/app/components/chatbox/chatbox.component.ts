8d9d4269affb39cdeed5f9cc144ef1ba96166c77
Veillette87 <alexandre.veillette@polymtl.ca>
2023-07-24 16:12:00 -0400

    add enter keydown on input field in chat

--- client/src/app/components/chatbox/chatbox.component.ts
+++ client/src/app/components/chatbox/chatbox.component.ts
@@ -7,15 +7,22 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class ChatboxComponent {
   @ViewChild('chatScroll') chatScroll!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef<HTMLButtonElement>;
+  @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
   constructor() {}
   message = '';
   chatMessages: string[][] = [];
   sendMessage() {
-    const chatMessage: string[] = ['user', this.message];
-    console.log(this.message);
-    this.message = '';
-    this.chatMessages.push(chatMessage);
-    this.chatScroll.nativeElement.scrollTop =
-      this.chatScroll.nativeElement.scrollHeight;
+    if (this.message) {
+      const chatMessage: string[] = ['user', this.message];
+      console.log(this.message);
+      this.message = '';
+      this.chatMessages.push(chatMessage);
+      this.chatScroll.nativeElement.scrollTop =
+        this.chatScroll.nativeElement.scrollHeight;
+      if (this.textarea) {
+        this.textarea.nativeElement.blur();
+      }
+    }
   }
 }
